503cbdbba2eba7872ff35efd17b9c7433dfe0bc2
Parth <20parthgupta03@gmail.com>
2025-05-13 23:49:17 +0530

    This Is intial UI with authentication setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+
+
+export default {
+  providers: [
+    {
+      domain: "https://polished-piranha-62.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+"use-client"
+
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs"
+
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
+  )
+}
+
+export default HomePage
