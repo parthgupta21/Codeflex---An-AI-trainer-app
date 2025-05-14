f63683dc46e28f8d6e71109c56c31da26929c17e
Parth <20parthgupta03@gmail.com>
2025-05-15 02:35:42 +0530

    Final Commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-img-element": "off",
+      "import/no-anonymous-default-export" : "off",
+    }
+  }
 ];
 
 export default eslintConfig;
